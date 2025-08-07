b976720c3c24a8b39d72af41bd71d80b0f2876fe
JHoo0118 <kik3078@gmail.com>
2025-08-07 22:27:20 +0900

    feat: Sentry 추가

--- apps/web/app/(dashboard)/page.tsx
+++ apps/web/app/(dashboard)/page.tsx
@@ -7,7 +7,7 @@ import { OrganizationSwitcher, SignOutButton, UserButton } from "@clerk/nextjs";
 
 export default function Page() {
   const users = useQuery(api.users.getMany);
-  const add = useMutation(api.users.add);
+  const addUser = useMutation(api.users.add);
 
   return (
     <div className="flex flex-col items-center justify-center min-h-svh">
@@ -15,7 +15,7 @@ export default function Page() {
       <UserButton />
       <OrganizationSwitcher hidePersonal={true} />
       <SignOutButton />
-      <Button onClick={() => add({ name: "hello" })}>Add</Button>
+      <Button onClick={() => addUser({ name: "hello" })}>Add</Button>
       <div className="max-w-sm w-full mx-auto">
         {users?.map((user) => <p key={user._id}>{user.name}</p>)}
       </div>
---
+++ apps/web/sentry.server.config.ts
@@ -0,0 +1,18 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://53263666b9f009d920338a498c6136cb@o363446.ingest.us.sentry.io/4509802972577792",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
