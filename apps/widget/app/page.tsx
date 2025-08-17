67bb3d1faebf779865ff1903bb8e9036ec15d4a7
JHoo0118 <kik3078@gmail.com>
2025-08-17 13:17:17 +0900

    feat: Widget Layout

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,28 +1,17 @@
 "use client";
 
-import { useVapi } from "@/modules/widget/hooks/use-vapi";
-import { Button } from "@workspace/ui/components/button";
+import { WidgetView } from "@/modules/widget/ui/views/widget-view";
 
-export default function Page() {
-  const {
-    startCall,
-    endCall,
-    isConnected,
-    isConnecting,
-    isSpeaking,
-    transcript,
-  } = useVapi();
-
-  return (
-    <div className="flex flex-col items-center justify-center min-h-svh max-w-md mx-auto w-full">
-      <Button onClick={() => startCall()}>Start call</Button>
-      <Button onClick={() => endCall()} variant="destructive">
-        End call
-      </Button>
-      <p>isConnected: {`${isConnected}`}</p>
-      <p>isConnecting: {`${isConnecting}`}</p>
-      <p>isSpeaking: {`${isSpeaking}`}</p>
-      <p>transcript: {JSON.stringify(transcript, null, 2)}</p>
-    </div>
-  );
+interface PageProps {
+  searchParams: Promise<{
+    organizationId: string;
+  }>;
 }
+
+const Page = async ({ searchParams }: PageProps) => {
+  const { organizationId } = await searchParams;
+
+  return <WidgetView organizationId={organizationId} />;
+};
+
+export default Page;
---
+++ apps/widget/modules/widget/ui/components/widget-header.tsx
@@ -0,0 +1,20 @@
+import { Button } from "@workspace/ui/components/button";
+import { cn } from "@workspace/ui/lib/utils";
+
+interface WidgetHeaderProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+export const WidgetHeader = ({ children, className }: WidgetHeaderProps) => {
+  return (
+    <header
+      className={cn(
+        "bg-gradient-to-b from-primary to-[#0b63f3] p-4 text-primary-foreground",
+        className
+      )}
+    >
+      {children}
+    </header>
+  );
+};
